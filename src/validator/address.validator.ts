64c815eb7e5a42a142067c60615cceeb9ce3262f
Sixtus <6ixtus6@gmail.com>
2025-03-06 09:32:42 +0100

    saved refactored changes:

--- src/validator/address.validator.ts
+++ src/validator/address.validator.ts
@@ -16,3 +16,17 @@ export const validateAddress = [
         next();
     }
 ];
+
+export const validateAddressPatch = [
+    check('street').notEmpty().withMessage('Street is required'),
+    check('city').notEmpty().withMessage('City is required'),
+    check('state').notEmpty().withMessage('State is required'),
+    check('zipCode').notEmpty().withMessage('Zip Code is required'),
+    (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(ApiResponse.code.bad_request).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
